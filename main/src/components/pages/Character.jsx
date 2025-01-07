0cb568213617212dae5f9417380dd4d6473362de
Joshua Loller <joshua.loller72@gmail.com>
2025-01-06 22:02:16 -0500

    added dice images

--- main/src/components/pages/Character.jsx
+++ main/src/components/pages/Character.jsx
@@ -1,3 +1,35 @@
+import "/App.css";
+import DiceImage1 from "./dice-imgs/dice1.png";
+import DiceImage2 from "./dice-imgs/dice2.png";
+import DiceImage3 from "./dice-imgs/dice3.png";
+import DiceImage4 from "./dice-imgs/dice4.png";
+import DiceImage5 from "./dice-imgs/dice5.png";
+import DiceImage6 from "./dice-imgs/dice6.png";
+import DiceImage7 from "./dice-imgs/dice7.png";
+import DiceImage8 from "./dice-imgs/dice8.png";
+import DiceImage9 from "./dice-imgs/dice9.png";
+import DiceImage10 from "./dice-imgs/dice10.png";
+import DiceImage11 from "./dice-imgs/dice11.png";
+import DiceImage12 from "./dice-imgs/dice12.png";
+import DiceImage13 from "./dice-imgs/dice13.png";
+import DiceImage14 from "./dice-imgs/dice14.png";
+import DiceImage15 from "./dice-imgs/dice15.png";
+import DiceImage16 from "./dice-imgs/dice16.png";
+import DiceImage17 from "./dice-imgs/dice17.png";
+import DiceImage18 from "./dice-imgs/dice18.png";
+import DiceImage19 from "./dice-imgs/dice19.png";
+import DiceImage20 from "./dice-imgs/dice20.png";
+
+function App()
+{
+    return(
+        <div className= "App">
+            
+        </div>
+    )
+}
+
+
 export default function Character() {
     return (
         <div className = "character">
