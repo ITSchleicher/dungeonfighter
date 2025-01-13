96cc3a0371183a4b6797c72b69f277693c150a38
Raumus204 <enacra204@gmail.com>
2025-01-12 21:01:53 -0600

    changed dist to build

--- server/server.js
+++ server/server.js
@@ -32,11 +32,11 @@ app.use(cors());
 app.use(express.json());
 // Save Character Route Middleware
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // For any other routes, serve the React app's index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
